133e480a4732e5f0b295edac209370ce03d9ea5a
Alejandro Almira <laboral@alejandroalmira.com>
2022-10-08 12:11:18 +0200

    build: entrypoint: add lint and test commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -37,26 +37,33 @@ except RedisError:
 END
 }
 
-until postgres_ready; do
-  >&2 echo "Waiting for PostgreSQL to become available..."
-  sleep 5
-done
->&2 echo "PostgreSQL is available"
+wait_other_containers() {
+	until postgres_ready; do
+		>&2 echo "Waiting for PostgreSQL to become available..."
+		sleep 5
+	done
+	>&2 echo "PostgreSQL is available"
 
-until redis_ready; do
-  >&2 echo "Waiting for Redis to become available..."
-  sleep 5
-done
->&2 echo "Redis is available"
+	until redis_ready; do
+		>&2 echo "Waiting for Redis to become available..."
+		sleep 5
+	done
+	>&2 echo "Redis is available"
+}
 
-python3 manage.py collectstatic --noinput
-python3 manage.py makemigrations
-python3 manage.py migrate
+django_operations() {
+	python3 manage.py collectstatic --noinput
+	python3 manage.py makemigrations
+	python3 manage.py migrate
+}
 
 cd /app
 
+
 case $1 in
 	"beat")
+		wait_other_containers ;\
+		django_operations ;\
 		celery \
 			--app meetupselector \
 			beat \
@@ -64,12 +71,16 @@ case $1 in
 			--scheduler django_celery_beat.schedulers:DatabaseScheduler
 		;;
 	"worker")
+		wait_other_containers ;\
+		django_operations ;\
 		celery \
 			--app meetupselector \
 			worker \
 			--loglevel INFO
 		;;
 	"server")
+		wait_other_containers ;\
+		django_operations ;\
 		if [ "$DJANGO_DEBUG" = "true" ]; then
 			gunicorn \
 				--reload \
@@ -85,7 +96,20 @@ case $1 in
 		fi
 		;;
 	"createsuperuser")
+		wait_other_containers ;\
+		django_operations ;\
 		python manage.py createsuperuser
+		;;
+	"test")
+		wait_other_containers ;\
+		django_operations ;\
+		pytest
+		;;
+	"lint")
+		isort --check-only meetupselector
+		black --check meetupselector
+		flake8 meetupselector
+		mypy meetupselector
 		;;
 	"*")
 		exec "$@"
