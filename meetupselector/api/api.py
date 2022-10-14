13674eb322b6b4a9cbcb6b1398a1fddce2401ea5
Alejandro Almira <laboral@alejandroalmira.com>
2022-10-15 01:41:59 +0200

    doc: api: add metadata to swagger doc

--- meetupselector/api/api.py
+++ meetupselector/api/api.py
@@ -2,13 +2,17 @@ from ninja import NinjaAPI
 
 from .routes import proposals_router, talks_router, users_router
 
-api = NinjaAPI()
+api = NinjaAPI(
+    title="MeetupSelector API",
+    version="alpha",
+    description="API for project https://github.com/pythonalicante/MeetupSelector/",
+)
 
-api.add_router("/proposals_app/", proposals_router)
-api.add_router("/talks_app/", talks_router)
-api.add_router("/users_app/", users_router)
+api.add_router("/proposals_app/", proposals_router, tags=["Proposals"])
+api.add_router("/talks_app/", talks_router, tags=["Talks"])
+api.add_router("/users_app/", users_router, tags=["Users"])
 
 
-@api.get("/healthcheck")
+@api.get("/healthcheck", tags=["Healthchecks"])
 def healthcheck(_):
     return {"healthy": True}
