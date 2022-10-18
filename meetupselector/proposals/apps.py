22b1f1b7cba4ed0b93af66931d3347e9e2e5e943
serfer2 <serfer2@protonmail.com>
2022-10-18 20:13:29 +0200

    fix proposal app namespace

--- meetupselector/proposals/apps.py
+++ meetupselector/proposals/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ProposalsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'proposals'
+    name = 'meetupselector.proposals'
