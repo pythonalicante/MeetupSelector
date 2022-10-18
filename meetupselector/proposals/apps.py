5ccb236ac708210dea0dcd0cbebe810a57c1e89c
Javi Lazaro <pichu_2707@hotmail.com>
2022-10-19 01:48:17 +0200

    Fix application name space

--- meetupselector/proposals/apps.py
+++ meetupselector/proposals/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class ProposalsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'proposals'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "meetupselector.proposals"
--- meetupselector/proposals/models.py
+++ meetupselector/proposals/models.py
@@ -1,3 +1,36 @@
-from django.db import models
+from email.policy import default
+from random import choices
+from tabnanny import verbose
+from unittest.util import _MAX_LENGTH
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class Difficulty(models.TextChoices):
+    EASY = "E", _("easy")
+    MEDIUM = "M", _("medium")
+    HARD = "H", _("hard")
+
+
+class Language(models.TextChoices):
+    EN_GB = "EN", _("english")
+    FR_FR = "FR", _("french")
+    ES_ES = "ES_ES", _("spain")
+    ES_EU = "ES_EU", _("basque")
+    ES_CA = "ES_CA", _("catalonian")
+    ES_GL = "ES_GL", _("galicia")
+
+class Proposal(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    description = models.TextField(verbose_name=_("description"))
+    difficulty = models.CharField(
+        verbose_name= _("difficulty"),
+        max_length=1,
+        choices=Difficulty.choices,
+        default=Difficulty.EASY,
+    )
+
