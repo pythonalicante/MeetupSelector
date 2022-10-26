99b7eec82577522de0c65198539b12a727d236dc
serfer2 <serfer2@protonmail.com>
2022-10-27 01:38:48 +0200

    Add input/output schemas for Topic, TAsk and Proposal

--- meetupselector/api/schemas/talks.py
+++ meetupselector/api/schemas/talks.py
@@ -1,9 +1,15 @@
 from ninja import ModelSchema
 
-from meetupselector.talks.models import Topic
+from meetupselector.talks.models import Talk, Topic
 
 
 class TopicRetrieveSchema(ModelSchema):
     class Config:
         model = Topic
-        model_fields = ["id", "name", "description"]
+        model_fields = ["id", "name", "description", "created_at", "updated_at"]
+
+
+class TalkListSchema(ModelSchema):
+    class Config:
+        model = Talk
+        model_fields = ["id", "name", "type", "created_at", "updated_at"]
