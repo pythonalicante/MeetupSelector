7d328044477e2dd65b16efc65c5ee2627c2fe740
Alejandro Almira <laboral@alejandroalmira.com>
2022-10-30 12:59:50 +0100

    feat: api: add topic schemas to retrieve and list

--- meetupselector/api/schemas/talks.py
+++ meetupselector/api/schemas/talks.py
@@ -3,6 +3,12 @@ from ninja import ModelSchema
 from meetupselector.talks.models import Topic
 
 
+class TopicListSchema(ModelSchema):
+    class Config:
+        model = Topic
+        model_fields = ["id", "name", "description"]
+
+
 class TopicRetrieveSchema(ModelSchema):
     class Config:
         model = Topic
