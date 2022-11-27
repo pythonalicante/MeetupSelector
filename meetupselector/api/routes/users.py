dbd8dc9093c168e474dd8b6410353049ca92bd3d
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2022-11-27 12:38:42 +0100

    Add User Delete route.

--- meetupselector/api/routes/users.py
+++ meetupselector/api/routes/users.py
@@ -3,8 +3,10 @@ from http import HTTPStatus
 from django.conf import settings
 from django.http import HttpRequest
 from django.urls import reverse
+from kombu.utils.functional import accepts_argument
 from ninja import Router
 from ninja.security import django_auth
+from pydantic import UUID4
 
 from meetupselector.api.schemas.users import LoginSchema
 from meetupselector.user.schemas import SignInSchema
@@ -43,3 +45,13 @@ def create_user(request: HttpRequest, credentials: SignInSchema):
 def activate_user(_):
     # TODO: Activate user and redirect to main page
     return HTTPStatus.OK, None
+
+
+@router.delete(
+    "/delete_user/{account_id}",
+    response={HTTPStatus.OK: None, HTTPStatus.NOT_FOUND: None, HTTPStatus.UNAUTHORIZED: None},
+    url_name="delete_user",
+)
+def delete_user(request, account_id: UUID4):
+    user = request.auth
+    return UserService.delete(account_id=account_id, user_id=user.id)
