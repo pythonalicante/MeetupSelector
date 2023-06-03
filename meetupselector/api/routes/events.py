5a02d5094921548ddc289890512f331e6a12f48e
Alejandro Almira <laboral@alejandroalmira.com>
2023-06-03 20:04:16 +0200

    Move events API to events file instead of proposals' one

--- meetupselector/api/routes/events.py
+++ meetupselector/api/routes/events.py
@@ -1,7 +1,13 @@
+import typing as t
+
 from ninja import Router
 from ninja.security import django_auth
 
-from meetupselector.api.schemas.proposals import EventCreateSchema, EventRetrieveSchema
+from meetupselector.api.schemas.proposals import (
+    EventCreateSchema,
+    EventListSchema,
+    EventRetrieveSchema,
+)
 from meetupselector.proposals.services import EventService
 
 router = Router(auth=django_auth)
@@ -17,3 +23,13 @@ def create_event(request, event: EventCreateSchema):
     if user.is_staff:
         return 201, EventService.create(**event.dict())
     return 401, None
+
+
+@router.get(
+    "/",
+    response={200: t.List[EventListSchema]},
+    url_name="create_list_event",
+    auth=None,
+)
+def list_events(request):
+    return 200, EventService.retrieve_all()
